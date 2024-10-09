282db6cf92eb842dd1f39328a5ca3136a72b976f
cristianquiroz6211 <141106618+cristianquiroz6211@users.noreply.github.com>
2024-10-09 17:53:25 -0500

    Proyecto

---
+++ ubico/src/test/java/co/com/ubico_ms/UbicoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ubico_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
